ccfac6e5fd629d4e135bc1684299b96952d21b4f
Mouhcin Qorra <74566930+Mouhcine-Qorra@users.noreply.github.com>
2021-12-30 03:29:43 +0100

    Update Pomodoro.py

--- Pomodoro.py
+++ Pomodoro.py
@@ -2,15 +2,22 @@ import time
 import datetime
 from plyer import notification
 
+
 total = start = datetime.datetime.now()
-end = start + datetime.timedelta(seconds=25)
+end = start + datetime.timedelta(minutes=25)
+notification.notify(title="Pomodoro Technique Activated!",
+                    message="Starting your 25min Work, Good luck",
+                    app_icon="pngme.ico",
+                    timeout=10
+                    )
+print(f"Starting Pomodoro Technique at {datetime.datetime.now().strftime('%H:%M:%S (%Y/%m/%d)')}")
 
 while True:
     if datetime.datetime.now() > end:
         notification.notify(title="Please take a short Break",
                             message="You Have studied for 25 Minutes, Take 5min Breaks! (Pomodoro Technique)",
                             app_icon="pngme.ico",
-                            timeout=10
+                            timeout=15
                             )
 
         # sleep 5min (5x60=300)
@@ -18,4 +25,9 @@ while True:
         print(f"You have Studied for {str(t_time).split('.')[0]}")
         time.sleep(300)
 
+        notification.notify(title="Get back to Work",
+                            message="Start your 25min Work again, Good luck",
+                            app_icon="pngme.ico",
+                            timeout=10
+                            )
         end = end + datetime.timedelta(minutes=25)
